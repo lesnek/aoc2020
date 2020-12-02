a39c3bfe7f517f2b0fae02bfe6db08144b31da5f
Lesnek <milan.lesnek@kiwi.com>
2020-12-02 14:56:28 +0100

    Add setup

--- day2/execute.ts
+++ day2/execute.ts
@@ -3,18 +3,22 @@ import { readFileSync } from "fs";
 const input = readFileSync("./data.txt", "utf-8").split("\n");
 
 const counter = (input: string[]) => {
-    var correct = 0;
+  var correct = 0;
 
-    for (const entry of input) {
-	if (entry == "") {break};
-       	const [range, letter, password] = entry.split(" ");
-    	const [min, max] = range.split("-").map(Number);
-	const rightLetters = (password.match(new RegExp(letter.charAt(0), "g")) || []).length;
-	if (min <= rightLetters && max >= rightLetters) {
-	    correct++;
-	}
+  for (const entry of input) {
+    if (entry == "") {
+      break;
     }
-    return correct;
-}
+    const [range, letter, password] = entry.split(" ");
+    const [min, max] = range.split("-").map(Number);
+    const rightLetters = (
+      password.match(new RegExp(letter.charAt(0), "g")) || []
+    ).length;
+    if (min <= rightLetters && max >= rightLetters) {
+      correct++;
+    }
+  }
+  return correct;
+};
 
 console.log(counter(input));
--- day2/execute2.ts
+++ day2/execute2.ts
@@ -3,20 +3,25 @@ import { readFileSync } from "fs";
 const input = readFileSync("./data.txt", "utf-8").split("\n");
 
 const counter = (input: string[]) => {
-    var correct = 0;
+  var correct = 0;
 
-    for (const entry of input) {
-	// weird, last character from split by \n is empty string
-	if (entry == "") {break};
-       	const [range, letter, password] = entry.split(" ");
-    	const [first, second] = range.split("-").map(Number);
-	const passIndex1 = password.charAt(first - 1); 
-	const passIndex2 = password.charAt(second - 1);
-	if ((passIndex1 == letter.charAt(0) || passIndex2 == letter.charAt(0)) && passIndex1 != passIndex2) {
-	    correct++;
-	}
+  for (const entry of input) {
+    // weird, last character from split by \n is empty string
+    if (entry == "") {
+      break;
     }
-    return correct;
-}
+    const [range, letter, password] = entry.split(" ");
+    const [first, second] = range.split("-").map(Number);
+    const passIndex1 = password.charAt(first - 1);
+    const passIndex2 = password.charAt(second - 1);
+    if (
+      (passIndex1 == letter.charAt(0) || passIndex2 == letter.charAt(0)) &&
+      passIndex1 != passIndex2
+    ) {
+      correct++;
+    }
+  }
+  return correct;
+};
 
 console.log(counter(input));
